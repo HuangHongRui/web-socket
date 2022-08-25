25f0e2c978280d805d95dbb3e58f64ebca8bfe45
HongRui Huang (Raine) <hhr464362353@gmail.com>
2022-08-25 10:49:45 +0800

    feat: 在线人员 | 样式

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useRef, useState } from "react";
-import { SYSMESSAGE_PLACEHOLDER, TLAKMESSAGE_PLACEHOLDER } from "./else";
+import { ONLINE_PLACEHOLDER, SYSMESSAGE_PLACEHOLDER, TLAKMESSAGE_PLACEHOLDER } from "./else";
 import socket from "./socket";
 
 const App = () => {
   const [sysMessage, setSysMessage] = useState("");
   const [talkMessage, setTalkMessage] = useState("");
+  const [onlineMessage, setOnlineMessage] = useState([]);
+
   const inputRef = useRef<any>();
   const wsRef = useRef<any>();
 
@@ -21,10 +23,17 @@ const App = () => {
     wsRef.current.addEventListener("message", (e: Event | any) => {
       console.log("接收Message数据", e);
       const { type, message } = JSON.parse(e.data);
-      type
-        ? setTalkMessage((data) => data + "\n" + message)
-        : setSysMessage((data) => data + "\n" + message);
-      console.log(type);
+      switch (type) {
+        case 0:
+          setSysMessage((data) => message + "\n" + data);
+          break;
+        case 1:
+          setTalkMessage((data) => message + "\n" + data);
+          break;
+        case 2:
+          setOnlineMessage(message?.join("\n"));
+          break;
+      }
     });
 
     // 4. WebSocket断开时触发
@@ -32,7 +41,6 @@ const App = () => {
       console.log("WebSocket断开时触发", e);
       // setMessage("WebSocket服务断开连接");
     });
-
   }, []);
 
   const fnSend = () => {
@@ -40,13 +48,17 @@ const App = () => {
     console.log("主动给WebSocket发送信息");
     wsRef.current.send(inputRef.current.value);
     inputRef.current.value = "";
-  }
+  };
 
   return (
     <div className="p-20 h-screen flex flex-col font-serif">
       <h1 className="text-center text-2xl mb-4">Hello Socket</h1>
       <div className="flex">
-        <input className="flex-1 px-10 mr-2 h-10 bg-red-50" placeholder="Say Something..." ref={inputRef} />
+        <input
+          className="flex-1 px-10 mr-2 h-10 bg-red-50"
+          placeholder="Say Something..."
+          ref={inputRef}
+        />
         <button
           className="w-40 h-10 rounded-md bg-sky-500 hover:bg-sky-700 ..."
           onClick={fnSend}
@@ -55,8 +67,26 @@ const App = () => {
         </button>
       </div>
       <div className="flex flex-1 mt-4 w-full">
-        <textarea placeholder={TLAKMESSAGE_PLACEHOLDER} readOnly className="flex-1 p-10 mt-4 bg-teal-100" value={talkMessage} />
-        <textarea placeholder={SYSMESSAGE_PLACEHOLDER} readOnly className="w-3/12 p-10 mt-4 bg-indigo-300" value={sysMessage} />
+        <textarea
+          placeholder={TLAKMESSAGE_PLACEHOLDER}
+          readOnly
+          className="flex-1 p-10 bg-teal-100"
+          value={talkMessage}
+        />
+        <div className="w-3/12 flex flex-col justify-between ml-4">
+          <textarea
+            placeholder={SYSMESSAGE_PLACEHOLDER}
+            readOnly
+            className="w-full p-10 flex-auto bg-indigo-300"
+            value={sysMessage}
+          />
+          <textarea
+            placeholder={ONLINE_PLACEHOLDER}
+            readOnly
+            className="w-full p-10 h-1/2 mt-4 bg-amber-100"
+            value={onlineMessage}
+          />
+        </div>
       </div>
     </div>
   );
--- src/App/socket.ts
+++ src/App/socket.ts
@@ -1,5 +1,6 @@
 const fnFun = () => {
-  const ws = new WebSocket("ws://localhost:8080");
+  // const ws = new WebSocket("ws://localhost:3000");
+  const ws = new WebSocket("ws://10.168.169.52:3000");
   return ws;
 };
 
